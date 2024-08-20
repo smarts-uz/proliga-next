6c6a7a4d4b323c987f839e4753b68bcbb0efe188
Muhammadamin <raufjonovmuhammadamin@gmail.com>
2024-08-20 18:23:39 +0500

    rules ui basic

---
+++ app/components/Rules/Rules.jsx
@@ -0,0 +1,21 @@
+import Gutter from '@/app/components/Gutter/Gutter'
+
+export default function Rules() {
+  return (
+    <Gutter>
+      <div className="text-white">
+  <span
+    className="text-center bg-primary  h-[55px] w-3/5 md:w-2/5 xl:w-1/5 rounded-md  text-black font-semibold text-xl sm:text[15px] md:text-[20px] xl:text-[25px] flex items-center justify-center">
+  UMUMIY QOIDALAR
+   </span>
+        <div className="text-white">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+          It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+          It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+          It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+          It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</div>
+      </div>
+    </Gutter>
+
+  )
+}
