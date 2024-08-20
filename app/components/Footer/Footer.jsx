25508bbf3f19bd6115ef99fcca62f2a10b1ef889
Muhammadamin <raufjonovmuhammadamin@gmail.com>
2024-08-20 12:56:07 +0500

    footer ui

--- app/components/Footer/Footer.jsx
+++ app/components/Footer/Footer.jsx
@@ -2,18 +2,24 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer className="bg-amber-600  w-full py-4 px-12">
-      <div className="flex justify-between">
-        <div className="text-left">
-          textjsjdbvj
+    <footer className="bg-black bg-opacity-90 justify-around  flex py-4 px-12 ">
+      <div className=" m-3">
+        <a href="URL" className="text-white">Про нас</a>
+      </div>
+      <div className="border-r-4 border-black "></div>
+      <div>
+        <div className=" m-3">
+          <a href="URL" className="text-white">политика</a>
         </div>
-        <div className="text-center">
-          textjsjdbvj
-        </div>
-        <div className="text-right">
-          textjsjdbvj
+        <div className=" m-3">
+          <a href="URL" className="text-white">конфиденциальности</a>
         </div>
       </div>
+      <div className="border-r-4 border-black"></div>
+      <div className="text-left m-3">
+        <a href="URL" className="text-white">Пользовательское соглашение</a>
+      </div>
+
     </footer>
   )
 }
