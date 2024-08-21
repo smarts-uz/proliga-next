6ea131fb9977e5c4d56c4d95c19d48e742594145
Ramz001 <rkenjaev1@gmail.com>
2024-08-21 17:18:42 +0500

    fix carousel

--- src/components/Carousel/index.jsx
+++ src/components/Carousel/index.jsx
@@ -1,101 +1,129 @@
-"use client";
-import React, { useState } from 'react'
+'use client'
+import { useState } from 'react'
+import Image from 'next/image'
+import Gutter from '../Gutter'
 
 const Carousel = () => {
-  const [currentIndex, setCurrentIndex] = useState(0);
+  const [currentIndex, setCurrentIndex] = useState(0)
 
   const slides = [
-    "https://via.placeholder.com/600x300/FF5733/FFFFFF?text=Slide+1",
-    "https://via.placeholder.com/600x300/33FF57/FFFFFF?text=Slide+2",
-    "https://via.placeholder.com/600x300/3357FF/FFFFFF?text=Slide+3"
-  ];
+    'https://via.placeholder.com/600x300/FF5733/FFFFFF?text=Slide+1',
+    'https://via.placeholder.com/600x300/33FF57/FFFFFF?text=Slide+2',
+    'https://via.placeholder.com/600x300/3357FF/FFFFFF?text=Slide+3',
+  ]
 
   const nextSlide = () => {
-    setCurrentIndex((currentIndex + 1) % slides.length);
-  };
+    setCurrentIndex((currentIndex + 1) % slides.length)
+  }
 
   const prevSlide = () => {
-    setCurrentIndex((currentIndex - 1 + slides.length) % slides.length);
-  };
+    setCurrentIndex((currentIndex - 1 + slides.length) % slides.length)
+  }
+
+  const increment = () => {
+    setCurrentIndex((prevState) => {
+      if (prevState === slides.length - 1) {
+        return 0
+      } else {
+        return prevState + 1
+      }
+    })
+  }
+
   return (
-    <div className="relative w-full  mx-auto ">
+    <Gutter className="relative bg-neutral-800">
       <div className="overflow-hidden">
-        <div className="flex transition-transform duration-500 ease-in-out"
-             style={{ transform: `translateX(-${currentIndex * 100}%)` }}>
+        <div
+          className="flex transition-transform duration-500 ease-in-out"
+          style={{ transform: `translateX(-${currentIndex * 100}%)` }}
+        >
           <div className="w-full flex-shrink-0">
-            <section className="h-auto bg-[#222222] md:px-[6rem] px-[1rem] py-[3rem] ">
-              <div className="bg-[#FFF400] md:w-1/3 w-auto pl-16 transform skew-x-45">
-                <h3 className='text-black capitalize text-[31px] italic font-bold'>umumiy qoidalar</h3>
+            <section className="h-auto">
+              <div className="w-auto -skew-x-12 transform bg-[#FFF400] pl-16 md:w-1/3">
+                <h3 className="text-[31px] font-bold capitalize italic text-black">
+                  umumiy qoidalar
+                </h3>
               </div>
-
-              <div className="xl:flex block items-center justify-center">
-                <div className='flex flex-wrap mt-[2rem] justify-center items-center'>
-                  <div className="flex flex-col flex-wrap justify-center items-center">
-
-                    <h2 className=' uppercase'>jamoa yig&apos;ing</h2>
-                    <p className=''>100 millionlik byudjetdan
-                      foydalaning va chempionatning eng yaxshifutbolchilaridan iborat jamoani to&apos;plang</p>
+              <div className="block items-center justify-center xl:flex">
+                <div className="mt-[2rem] flex flex-wrap items-center justify-center">
+                  <div className="flex flex-col flex-wrap items-center justify-center">
+                    <h2 className="uppercase">jamoa yig&apos;ing</h2>
+                    <p className="">
+                      100 millionlik byudjetdan foydalaning va chempionatning
+                      eng yaxshifutbolchilaridan iborat jamoani to&apos;plang
+                    </p>
                   </div>
-
-                  <div className="relative sm:w-[430px] sm:h-[430px] w-[230px] h-[230px]">
+                  <div className="relative h-[230px] w-[230px] sm:h-[430px] sm:w-[430px]">
                     <img
-                      className="absolute left-1/2 top-0 sm:w-auto sm:h-auto w-[120px] h-[120px]  transform -translate-x-1/2"
-                      src='/images/promotion-3.png' />
+                      className="absolute left-1/2 top-0 h-[120px] w-[120px] -translate-x-1/2 transform sm:h-auto sm:w-auto"
+                      src="/images/promotion-3.png"
+                    />
                     <img
-                      className="absolute left-1/2 bottom-0 sm:w-auto sm:h-auto w-[120px] h-[120px] transform -translate-x-1/2"
-                      src='/images/promotion-4.png' />
-                    <img className="absolute top-1/2 left-0 sm:w-auto sm:h-auto w-[120px] h-[120px] transform -translate-y-1/2"
-                      src='/images/promotion-5.png' />
+                      className="absolute bottom-0 left-1/2 h-[120px] w-[120px] -translate-x-1/2 transform sm:h-auto sm:w-auto"
+                      src="/images/promotion-4.png"
+                    />
                     <img
-                      className="absolute top-1/2 right-0 sm:w-auto sm:h-auto w-[120px] h-[120px] transform -translate-y-1/2"
-                      src='/images/promotion-6.png' />
+                      className="absolute left-0 top-1/2 h-[120px] w-[120px] -translate-y-1/2 transform sm:h-auto sm:w-auto"
+                      src="/images/promotion-5.png"
+                    />
+                    <img
+                      className="absolute right-0 top-1/2 h-[120px] w-[120px] -translate-y-1/2 transform sm:h-auto sm:w-auto"
+                      src="/images/promotion-6.png"
+                    />
                   </div>
                 </div>
-
-                <div className="flex justify-center items-center">
+                <div className="flex items-center justify-center">
                   <img
-                    className="2xl:max-w-[865px] mx-auto 2xl:max-h-[648px] lg:max-w-[600px] lg:max-h-[448px] max-w-[400px] max-h-[248px] w-auto h-auto"
-                    src="/images/promotion-1.png" />
+                    className="mx-auto h-auto max-h-[248px] w-auto max-w-[400px] lg:max-h-[448px] lg:max-w-[600px] 2xl:max-h-[648px] 2xl:max-w-[865px]"
+                    src="/images/promotion-1.png"
+                  />
                 </div>
               </div>
             </section>
           </div>
           <div className="w-full flex-shrink-0">
-            <section className="flex h-full bg-[#222222] md:px-[6rem] px-[1rem] py-[3rem] ">
-              <div className={"mt-24 h-full"}>
+            <section className="flex h-full bg-[#222222] px-[1rem] py-[3rem] md:px-[6rem]">
+              <div className={'mt-24 h-full'}>
                 <h2>BALLARNI YIG’ING</h2>
                 <p className="max-w-[651px]">
-                  Har bir o&apos;yinchi haqiqiy o&apos;yinlarda qilgan harakatlari uchun
-                  ochko oladi yoki yo&apos;qotadi. Ballar soni o&apos;yinchining roliga qarab
-                  farq qilishi mumkin. Masalan, himoyachi gol uchun 6 ochko, hujumchi esa 4 ochko
-                  oladi
+                  Har bir o&apos;yinchi haqiqiy o&apos;yinlarda qilgan
+                  harakatlari uchun ochko oladi yoki yo&apos;qotadi. Ballar soni
+                  o&apos;yinchining roliga qarab farq qilishi mumkin. Masalan,
+                  himoyachi gol uchun 6 ochko, hujumchi esa 4 ochko oladi
                 </p>
               </div>
-              <div className={"ml-96 mt-28"}>
+              <div className={'ml-96 mt-28'}>
                 <img src="/images/promotion-2.png" alt="" />
               </div>
             </section>
           </div>
-
         </div>
       </div>
-      <button onClick={prevSlide}
-              className="absolute top-1/2 left-0 transform -translate-y-1/2  bg-opacity-50 text-white px-4 py-2">
-        <svg className="w-6 h-6 text-secondary dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
-             fill="none" viewBox="0 0 8 14">
-          <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
-                d="M7 1 1.3 6.326a.91.91 0 0 0 0 1.348L7 13" />
-        </svg>
+      <button
+        onClick={prevSlide}
+        className="absolute left-0 top-1/2 -translate-y-1/2 transform bg-opacity-50 px-4 py-2 text-white"
+      >
+        <Image
+          src="/icons/arrow-down.svg"
+          alt="arrow"
+          width={32}
+          height={32}
+          className="rotate-90"
+        />
       </button>
-      <button onClick={nextSlide}
-              className="absolute top-1/2 right-0 transform -translate-y-1/2  bg-opacity-50 text-white px-4 py-2">
-        <svg className="w-6 h-6 text-secondary dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
-             fill="none" viewBox="0 0 8 14">
-          <path stroke="currentColor" strokel-linecap="round" strokeLinejoin="round" strokeWidth="2"
-                d="m1 13 5.7-5.326a.909.909 0 0 0 0-1.348L1 1" />
-        </svg>
+      <button
+        onClick={nextSlide}
+        className="absolute right-0 top-1/2 -translate-y-1/2 transform bg-opacity-50 px-4 py-2 text-white"
+      >
+        <Image
+          src="/icons/arrow-down.svg"
+          className="-rotate-90"
+          alt="arrow"
+          width={32}
+          height={32}
+        />
       </button>
-    </div>
+    </Gutter>
   )
 }
 export default Carousel
