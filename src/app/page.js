68b6d818fb0fed5f45c760e5c4e96571c4bf272b
Ahmad <tulkinxojaevaxad@gmail.com>
2024-08-21 11:05:45 +0300

    responsive

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
 import Hero from "../components/Hero"
+import Promotions from '@/src/components/Promotions'
 
 export default function Home() {
   return (
     <>
       <Hero />
+      <Promotions/>
     </>
   )
 }
