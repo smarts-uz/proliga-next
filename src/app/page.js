f9caaaa536aa36a794baf62767e107ebf9ddef7e
Muhammadamin <raufjonovmuhammadamin@gmail.com>
2024-08-21 13:39:44 +0500

    Rules modul

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
 import Hero from './components/Hero'
+import Rules from '@/src/app/components/Rules'
 
 export default function Home() {
   return (
     <>
       <Hero />
+      <Rules />
     </>
   )
 }
