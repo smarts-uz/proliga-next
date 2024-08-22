f9ccff4ddc7da67341174a29ba4440ff790fea3a
Ahmad <tulkinxojaevaxad@gmail.com>
2024-08-22 08:22:07 +0300

    prizes section

--- src/components/Promotions/index.jsx
+++ src/components/Promotions/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import CreateTeam from '@/src/components/Promotions/CreateTeam'
 import GetPoints from '@/src/components/Promotions/GetPoints'
 import Transfers from '@/src/components/Promotions/Transfers'
+import Prizes from '@/src/components/Promotions/Prizes'
 
 const  Promotions = () => {
     return (
@@ -9,6 +10,7 @@ const  Promotions = () => {
         <CreateTeam/>
         <GetPoints/>
          <Transfers/>
+        <Prizes/>
       </>
     )
 }
