7db07e561e65b115e1750b9a82747d5415170897
Ramz001 <rkenjaev1@gmail.com>
2024-08-25 17:31:06 +0500

    ui for tournament and stats tabs play route

--- src/app/(pages)/play/components/Journal/index.jsx
+++ src/app/(pages)/play/components/Journal/index.jsx
@@ -3,34 +3,56 @@ import Gutter from '../../../../../components/Gutter'
 const Journal = () => {
   return (
     <Gutter>
-      <section className="flex w-2/3 flex-col rounded-2xl bg-black p-6 text-neutral-300">
-        <div className="">
-          {data.map((item, index) => (
-            <div
-              key={index}
-              className="flex gap-4 border-b border-neutral-700 py-2 text-neutral-200"
-            >
-              <p>{item.date}</p>
-              <p>{item.text}</p>
-            </div>
-          ))}
-        </div>
-        <div className="flex items-center justify-between py-4">
-          <button className="capitalize text-white hover:underline">
-            Oldigisi
-          </button>
-          <div className="flex gap-2">
-            <button className="rounded border bg-white px-2 py-1 text-black">
-              1
-            </button>
-            <button className="rounded border px-2 py-1">2</button>
-            <button className="rounded border px-2 py-1">3</button>
+      <div className='flex gap-4'>
+        <section className="flex w-2/3 flex-col rounded-2xl bg-black p-6 text-neutral-300">
+          <div className="">
+            {data.map((item, index) => (
+              <div
+                key={index}
+                className="flex gap-4 border-b border-neutral-700 py-2 text-neutral-200"
+              >
+                <p>{item.date}</p>
+                <p>{item.text}</p>
+              </div>
+            ))}
           </div>
-          <button className="capitalize text-white hover:underline">
-            Keyingisi
-          </button>
+          <div className="flex items-center justify-between py-4">
+            <button className="capitalize text-white hover:underline">
+              Oldigisi
+            </button>
+            <div className="flex gap-2">
+              <button className="rounded border bg-white px-2 py-1 text-black">
+                1
+              </button>
+              <button className="rounded border px-2 py-1">2</button>
+              <button className="rounded border px-2 py-1">3</button>
+            </div>
+            <button className="capitalize text-white hover:underline">
+              Keyingisi
+            </button>
+          </div>
+        </section>
+        <div className="flex h-min w-1/3 flex-col gap-8">
+          <div className="w-full rounded-xl bg-black p-8 text-neutral-100">
+            <h3 className="text-xl font-bold">ENG KUCHLI TOP 3 - JAMOALAR</h3>
+            <div className="mt-4 grid grid-cols-3 gap-2">
+              <div className="size-28 rounded-xl bg-white"></div>{' '}
+              <div className="size-28 rounded-xl bg-white"></div>
+              <div className="size-28 rounded-xl bg-white"></div>
+            </div>
+          </div>
+          <div className="w-full rounded-xl bg-black p-8 text-neutral-100">
+            <h3 className="text-xl font-bold">
+              ENG KUCHLI TOP 3 - Futbolchilar
+            </h3>
+            <div className="mt-4 grid grid-cols-3 gap-2">
+              <div className="size-28 rounded-xl bg-white"></div>{' '}
+              <div className="size-28 rounded-xl bg-white"></div>
+              <div className="size-28 rounded-xl bg-white"></div>
+            </div>
+          </div>
         </div>
-      </section>
+      </div>
     </Gutter>
   )
 }
--- src/app/(pages)/play/components/Tournament/index.jsx
+++ src/app/(pages)/play/components/Tournament/index.jsx
@@ -21,39 +21,58 @@ const Tournament = () => {
 
   return (
     <Gutter>
-      <div className="h-min min-h-[36rem] max-h-[36rem] table-auto overflow-x-auto rounded-2xl bg-neutral-950 p-6 text-neutral-200 md:w-2/3">
-        <table class="w-full table-auto border-spacing-0">
-          <thead>
-            <tr>
-              <th>Player</th>
-              <th>Position</th>
-              <th>Price</th>
-              <th>Club</th>
-              <th></th>
-            </tr>
-          </thead>
-          <tbody>
-            {players.map((player) => (
-              <tr key={player.id} className="border-spacing-0">
-                <td className="w-24 truncate">{player.name}</td>
-                <td>{player.position}</td>
-                <td>{player.price}</td>
-                <td>{player.club.name}</td>
-                <button>
-                  <Image
-                    src="/icons/plus.svg"
-                    alt="add player"
-                    width={24}
-                    height={24}
-                    className="filter-primary"
-                  />
-                </button>
+      <section className="flex gap-4">
+        <div className="h-min max-h-[36rem] min-h-[36rem] table-auto overflow-x-auto rounded-2xl bg-neutral-950 p-6 text-neutral-200 md:w-2/3">
+          <table class="w-full table-auto border-spacing-0">
+            <thead>
+              <tr>
+                <th>Player</th>
+                <th>Position</th>
+                <th>Price</th>
+                <th>Club</th>
+                <th></th>
               </tr>
-            ))}
-          </tbody>
-        </table>
-        <div className="h-4" />
-      </div>
+            </thead>
+            <tbody>
+              {players.map((player) => (
+                <tr key={player.id} className="border-spacing-0">
+                  <td className="w-24 truncate">{player.name}</td>
+                  <td>{player.position}</td>
+                  <td>{player.price}</td>
+                  <td>{player.club.name}</td>
+                  <button>
+                    <Image
+                      src="/icons/plus.svg"
+                      alt="add player"
+                      width={24}
+                      height={24}
+                      className="filter-primary"
+                    />
+                  </button>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+        <div className="h-min w-1/3 flex flex-col gap-8">
+          <div className="w-full rounded-xl bg-black p-8 text-neutral-100">
+            <h3 className="text-xl font-bold">ENG KUCHLI TOP 3 - JAMOALAR</h3>
+            <div className="mt-4 grid grid-cols-3 gap-2">
+              <div className="size-28 rounded-xl bg-white"></div>{' '}
+              <div className="size-28 rounded-xl bg-white"></div>
+              <div className="size-28 rounded-xl bg-white"></div>
+            </div>
+          </div>
+          <div className="w-full rounded-xl bg-black p-8 text-neutral-100">
+            <h3 className="text-xl font-bold">ENG KUCHLI TOP 3 - Futbolchilar</h3>
+            <div className="mt-4 grid grid-cols-3 gap-2">
+              <div className="size-28 rounded-xl bg-white"></div>{' '}
+              <div className="size-28 rounded-xl bg-white"></div>
+              <div className="size-28 rounded-xl bg-white"></div>
+            </div>
+          </div>
+        </div>
+      </section>
     </Gutter>
   )
 }
