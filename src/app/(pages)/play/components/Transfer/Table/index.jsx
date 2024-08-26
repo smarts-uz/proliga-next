476eaecd9237af367b3a1f8355a14dd99def8e8a
Ramz001 <rkenjaev1@gmail.com>
2024-08-26 12:43:30 +0500

    table mvp

--- src/app/(pages)/play/components/Transfer/Table/index.jsx
+++ src/app/(pages)/play/components/Transfer/Table/index.jsx
@@ -12,24 +12,26 @@ import {
   createColumnHelper,
 } from '@tanstack/react-table'
 import React from 'react'
-import { superData } from './playersData'
 import { supabase } from '../../../../../lib/supabaseClient'
 
 const columnHelper = createColumnHelper()
 
 const columns = [
   columnHelper.accessor('name', {
+    accessorKey: 'name',
     cell: (info) => info.getValue(),
     footer: (info) => info.column.id,
     header: () => <span className="w-1/2">Name</span>,
   }),
   columnHelper.accessor((row) => row.position, {
+    accessorFn: row => row.position,
     id: 'position',
     cell: (info) => <i>{info.getValue()}</i>,
     header: 'Position',
     footer: (info) => info.column.id,
   }),
   columnHelper.accessor('price', {
+    accessorKey: 'price',
     header: 'Price',
     cell: (info) => info.renderValue(),
     footer: (info) => info.column.id,
@@ -39,6 +41,7 @@ const columns = [
   //   footer: (info) => info.column.id,
   // }),
   columnHelper.accessor('club.name', {
+    accessorKey: 'club.name',
     header: 'Club',
     footer: (info) => info.column.id,
     meta: {
@@ -55,7 +58,7 @@ function TransferTable() {
   const [players, setPlayers] = React.useState([])
   const [pagination, setPagination] = React.useState({
     pageIndex: 0,
-    pageSize: 10,
+    pageSize: 14,
   })
 
   const [data, _setData] = React.useState([])
@@ -65,7 +68,7 @@ function TransferTable() {
       const { data, error } = await supabase
         .from('player')
         .select('id, name, position, club(name), price')
-        .limit(220)
+        .limit(240)
       if (error) return toast.error(error.message)
       if (data?.length > 0) _setData(data)
     }
@@ -87,21 +90,39 @@ function TransferTable() {
   })
 
   return (
-    <div className="max-h-[40rem] w-1/2 overflow-y-auto rounded-xl bg-black p-6 text-neutral-200">
-      <table className="h-96 table-fixed">
-        <thead>
-          {table.getHeaderGroups().map((headerGroup) => (
-            <tr className="w" key={headerGroup.id}>
-              {headerGroup.headers.map((header) => (
-                <th key={header.id}>
-                  {header.isPlaceholder
-                    ? null
-                    : flexRender(
+    <div className="h-min text-sm w-1/2 overflow-y-auto rounded-xl bg-black p-6 text-neutral-200">
+      <table className="w-full table-auto">
+      <thead>
+          {table.getHeaderGroups().map(headerGroup => (
+            <tr key={headerGroup.id}>
+              {headerGroup.headers.map(header => {
+                return (
+                  <th key={header.id} colSpan={header.colSpan}>
+                    <div
+                      {...{
+                        className: header.column.getCanSort()
+                          ? 'cursor-pointer select-none'
+                          : '',
+                        onClick: header.column.getToggleSortingHandler(),
+                      }}
+                    >
+                      {flexRender(
                         header.column.columnDef.header,
                         header.getContext()
                       )}
-                </th>
-              ))}
+                      {{
+                        asc: ' 🔼',
+                        desc: ' 🔽',
+                      }[header.column.getIsSorted()] ?? null}
+                      {header.column.getCanFilter() ? (
+                        <div>
+                          <Filter column={header.column} table={table} />
+                        </div>
+                      ) : null}
+                    </div>
+                  </th>
+                )
+              })}
             </tr>
           ))}
         </thead>
@@ -139,15 +160,16 @@ function TransferTable() {
           Page {table.getState().pagination.pageIndex + 1} of{' '}
           {table.getPageCount()}
         </div>
-        <div className='flex gap-4'>
+        <div className="flex gap-2">
           <button
             onClick={() => table.previousPage()}
             isDisabled={!table.getCanPreviousPage()}
-            className='border rounded-sm py-1 px-2'
+            className="rounded-sm border px-2 py-1"
           >
             {'<'}
           </button>
           <button
+            className="rounded-sm border px-2 py-1"
             isDisabled={!table.getCanNextPage()}
             onClick={() => table.nextPage()}
           >
@@ -158,5 +180,55 @@ function TransferTable() {
     </div>
   )
 }
+
+function Filter({
+  column,
+  table,
+}) {
+  const firstValue = table
+    .getPreFilteredRowModel()
+    .flatRows[0]?.getValue(column.id)
+
+  const columnFilterValue = column.getFilterValue()
+
+  return typeof firstValue === 'number' ? (
+    <div className="flex text-black space-x-2" onClick={e => e.stopPropagation()}>
+      <input
+        type="number"
+        value={(columnFilterValue)?.[0] ?? ''}
+        onChange={e =>
+          column.setFilterValue((old) => [
+            e.target.value,
+            old?.[1],
+          ])
+        }
+        placeholder={`Min`}
+        className="w-24 text-black border shadow rounded"
+      />
+      <input
+        type="number"
+        value={(columnFilterValue )?.[1] ?? ''}
+        onChange={e =>
+          column.setFilterValue((old) => [
+            old?.[0],
+            e.target.value,
+          ])
+        }
+        placeholder={`Max`}
+        className="w-24 text-black border shadow rounded"
+      />
+    </div>
+  ) : (
+    <input
+      className="w-36 border text-black shadow rounded"
+      onChange={e => column.setFilterValue(e.target.value)}
+      onClick={e => e.stopPropagation()}
+      placeholder={`Search...`}
+      type="text"
+      value={(columnFilterValue ?? '')}
+    />
+  )
+}
+
 
 export default TransferTable
