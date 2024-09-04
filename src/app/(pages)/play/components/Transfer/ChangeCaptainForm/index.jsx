ed09a9c316bb9f48556c6bda313e60c3cecca4bf
Ramz001 <rkenjaev1@gmail.com>
2024-09-04 18:04:56 +0500

    captain persist fix

--- src/app/(pages)/play/components/Transfer/ChangeCaptainForm/index.jsx
+++ src/app/(pages)/play/components/Transfer/ChangeCaptainForm/index.jsx
@@ -46,6 +46,7 @@ const ChangeCaptainForm = () => {
           name="formation"
           id="formation"
           onClick={(e) => dispatch(setCapitan(e.target.value))}
+          value={team.captain_id ?? null}
           className="w-48 -skew-x-12 rounded-sm border border-neutral-900 bg-neutral-950 p-2 font-semibold text-neutral-200 outline-none"
         >
           <option
