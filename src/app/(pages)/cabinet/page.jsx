4a37618b5a132749959134f244f5edbec5e61a47
mukhtarovich-dev <dilbekk3@gmail.com>
2024-09-13 03:36:39 -0700

    cabinet ui fix

--- src/app/(pages)/cabinet/page.jsx
+++ src/app/(pages)/cabinet/page.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable @next/next/no-img-element */
+/* eslint-disable react-hooks/exhaustive-deps */
 'use client'
 
 import { useEffect, useState } from 'react'
@@ -14,13 +16,21 @@ function Page() {
   const { updateData, error, isLoading } = useUpdateUserData()
   const { uploadFile, isLoading: uploadLoading, } = useUploadFile()
   const {downloadFile,img}=useDownloadFile()  
+  const [photo, setPhoto] = useState(null);
+  const [imgPreview, setImgPreview] = useState(img);
+  const handleFileChange = (e) => {
+    const file = e.target.files[0]; 
+    setImgPreview(URL.createObjectURL(file));
+    setPhoto(file);
+  };
+  console.log(imgPreview);
+  
+  let rasm = document.getElementById('img')
   const uploadImage = async () => {
-    let rasm = document.getElementById('img')
     await uploadFile(rasm.files[0])
-    
-    window.location.reload()
-
-  }
+    // window.location.reload()
+  }  
+  
   const formik = useFormik({
     initialValues: {
       firstName: '',
@@ -50,16 +60,36 @@ function Page() {
       )
     } catch (error) {}
   }
+  const auth_container='mx-4 flex w-full flex-col gap-4 rounded-xl bg-neutral-950 px-6 py-8 max-w-72 xs:max-w-[36rem] sm:max-w-[40rem] shadow shadow-neutral-500 md:p-8;'
+  const auth_input='h-10 w-full rounded border border-yellow-700 bg-neutral-900 px-2 py-1 text-sm text-neutral-200 placeholder:text-neutral-500 md:text-base'
   return (
     <Gutter>
       <div className="z-10 mt-24 flex min-h-svh items-center justify-center py-6 text-gray-200 ">
         <form
           onSubmit={formik.handleSubmit}
-          className="auth-container w-screen max-w-[80rem] items-center justify-center gap-2"
+          className={" max-w-[80rem] items-center justify-center gap-2 "+auth_container}
         >
           <h3 className="flex gap-4 text-xl">Ma&apos;lumot</h3>
+          <label
+                className=" my-2 block text-sm font-bold text-neutral-300"
+                htmlFor="img"
+              >
+                <div className='relative'>
+                <img src={imgPreview?imgPreview:img} className='rounded-full xl:size-56 md:size-40 lg:size-48 sm:size-36 xs:size-32 border-neutral-50 bg-neutral-300 border-2'  width={100} height={100} key={img} alt="img" />
+                <span style={{ left: "calc(50% - 24px)", top: 'calc(50% - 24px)' }} className='absolute bg-green-600 block rounded-full w-12 h-12 top-0 text-black'>
+                </span>
+                </div>
+              </label>
+              <input type="file"   onChange={handleFileChange} id="img" className="hidden" />
+              <button
+                type="button"
+                className={photo?"mt-4 w-full rounded-sm border border-primary bg-neutral-900 py-3 font-semibold transition-all hover:bg-black":('hidden')}
+                onClick={() => uploadImage()}
+              >
+                Surat qo&apos;shish
+              </button>
           <div className="flex">
-            <div className="my-4 max-w-72 xs:max-w-96 sm:max-w-[36rem]">
+            <div className="my-4 min-w-72 xs:max-w-96 sm:max-w-[36rem]">
               <label
                 className="my-2 block text-sm font-bold text-neutral-300"
                 htmlFor="email"
@@ -72,7 +102,7 @@ function Page() {
                 type="text"
                 disabled
                 placeholder={'example@email.com'}
-                className="auth-input"
+                className={auth_input}
                 onChange={formik.handleChange}
                 value={formik.values.email}
               />
@@ -87,7 +117,7 @@ function Page() {
                 id="firstName"
                 name="firstName"
                 type="text"
-                className="auth-input"
+                className={auth_input}
                 onChange={formik.handleChange}
                 value={formik.values.firstName}
               />
@@ -102,7 +132,7 @@ function Page() {
                 name="lastName"
                 placeholder="Familiya"
                 type="text"
-                className="auth-input"
+                className={auth_input}
                 onChange={formik.handleChange}
                 value={formik.values.lastName}
               />
@@ -118,7 +148,7 @@ function Page() {
                 name="middleName"
                 placeholder="Sharif"
                 type="text"
-                className="auth-input"
+                className={auth_input}
                 onChange={formik.handleChange}
                 value={formik.values.middleName}
               />
@@ -136,21 +166,21 @@ function Page() {
                 type="date"
                 cols={1}
                 rows={5}
-                className="auth-input min-h-64 gap-4"
+                className={auth_input+" min-h-64 gap-4"}
                 onChange={formik.handleChange}
                 value={formik.values.bio}
               />
               <div className="py-4 lg:flex">
                 <div>
                   <label
-                    className="mx-2 my-2 block text-sm font-bold text-neutral-300"
+                    className="mx-2 my-2  block text-sm font-bold text-neutral-300"
                     htmlFor="gender"
                   >
                     Jins
                   </label>
                   <select
                     id="gender"
-                    className="auth-input mx-2 lg:w-64"
+                    className={auth_input+"mx-2 mr-2 lg:w-64"}
                     onChange={formik.handleChange}
                     value={formik.values.gender}
                   >
@@ -170,7 +200,7 @@ function Page() {
                   <DatePicker
                     id="birthdate"
                     selected={startDate}
-                    className="auth-input mx-2 w-[288px] xs:w-[384px] sm:w-[576px] lg:w-72"
+                    className={auth_input+"mx-2 w-[288px] xs:w-[384px] sm:w-[576px] lg:w-72"}
                     onChange={(date) => setStartDate(date)}
                   />
                 </div>
@@ -186,21 +216,7 @@ function Page() {
               >
                 Tahrirlash
               </button>
-              <label
-                className=" my-2 block text-sm font-bold text-neutral-300"
-                htmlFor="img"
-              >
-                Profil uchun surat
-                <img src={img} width={100} height={100} key={img} alt="img" />
-              </label>
-              <input type="file" id="img" className="hidden" />
-              <button
-                type="button"
-                className="mt-4 w-full rounded-sm border border-primary bg-neutral-900 py-3 font-semibold transition-all hover:bg-black"
-                onClick={() => uploadImage()}
-              >
-                Surat qo&apos;shish
-              </button>
+             
             </div>
           </div>
         </form>
