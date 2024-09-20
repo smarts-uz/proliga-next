1d3c6e66ffef1623821f3485dda35fa13c9647b7
Muhammadamin <raufjonovmuhammadamin@gmail.com>
2024-09-20 23:55:13 +0500

    add Translation to teal and some button , navbar

--- src/components/Navbar/Notification/Notification.jsx
+++ src/components/Navbar/Notification/Notification.jsx
@@ -1,7 +1,9 @@
 'use client'
 import { motion } from 'framer-motion'
+import { useTranslation } from 'react-i18next'
 
 const Notification = () => {
+  const { t } = useTranslation()
   return (
     <motion.section
       initial={{ opacity: 0, y: -20 }}
@@ -9,9 +11,9 @@ const Notification = () => {
       exit={{ opacity: 1, y: -20 }}
       className="absolute right-0 top-12 flex h-full min-h-80 w-80 flex-col gap-4 rounded-md bg-neutral-900 p-4 shadow-sm shadow-neutral-500"
     >
-      <h3 className="font-bold">Xabarnomalar</h3>
+      <h3 className="font-bold">{t("Xabarnomalar")}</h3>
       <div className="text-neutral-200">
-        <p className="text-center text-sm">Hozicha habarlar yoq!</p>
+        <p className="text-center text-sm">{t("Hozicha habarlar yoq")}</p>
       </div>
     </motion.section>
   )
