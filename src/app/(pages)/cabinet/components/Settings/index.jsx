c5d77e6cdfefef9c0fabad2714e7bd7e7e7a7e98
Muhammadamin <raufjonovmuhammadamin@gmail.com>
2024-09-30 11:21:29 +0500

    fix

--- src/app/(pages)/cabinet/components/Settings/index.jsx
+++ src/app/(pages)/cabinet/components/Settings/index.jsx
@@ -27,11 +27,11 @@ const CabinetSettingsTab = () => {
   console.log(file, firstName, lastName, middleName, abous, date, gender)
 
   return (
-    <section className="h-full w-full flex-1 rounded-xl  bg-neutral-900 bg-opacity-90 p-4 lg:h-auto xl:p-6">
+    <section className="h-full w-full flex-1 rounded-xl bg-neutral-900 bg-opacity-90 p-4 lg:h-auto xl:p-6">
       <form className="flex flex-col gap-2">
         <section className="flex w-full flex-col gap-4 lg:flex-row">
           <div className="cursor-pointer space-y-1">
-            <p className="text-neutral-200">Sizning Rasmingiz:</p>
+            <p className="text-neutral-200">{t('Sizning Rasmingiz')}:</p>
             <label
               htmlFor="img"
               className="flex size-32 flex-col items-center justify-center gap-1 rounded-xl border border-dashed border-neutral-300 bg-gradient-to-r from-neutral-800 to-stone-900 p-6 transition-all hover:from-neutral-900 hover:to-stone-900"
@@ -51,7 +51,7 @@ const CabinetSettingsTab = () => {
                 className="hidden"
               />
               <p className="break-words text-center text-xs text-neutral-300">
-                Rasmni yuklang
+                {t('Rasmni yuklash')}
               </p>
             </label>
           </div>
