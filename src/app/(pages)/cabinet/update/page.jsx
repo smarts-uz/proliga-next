8ec424a29714b4acb7a7144ba33dac33826432bc
Ramz001 <rkenjaev1@gmail.com>
2024-10-19 16:10:54 +0500

    getting rid of unused deps

--- src/app/(pages)/cabinet/update/page.jsx
+++ src/app/(pages)/cabinet/update/page.jsx
@@ -1,103 +1,104 @@
-'use client'
+// 'use client'
 
-import Gutter from 'components/Gutter'
-import { useFormik } from 'formik'
-import { useUpdatePassword } from 'app/hooks/auth/useUpdatePassword/useUpdatePassword'
-import { useState } from 'react'
-import { useTranslation } from 'react-i18next'
-const UpdatePassword = () => {
-  const { updatePassword, error, isLoading } = useUpdatePassword()
-  const [showPassword, setShowPassword] = useState(false)
+// import Gutter from 'components/Gutter'
+// import { useFormik } from 'formik'
+// import { useUpdatePassword } from 'app/hooks/auth/useUpdatePassword/useUpdatePassword'
+// import { useState } from 'react'
+// import { useTranslation } from 'react-i18next'
 
-  const formik = useFormik({
-    initialValues: {
-      oldPassword: '',
-      newPassword: '',
-      confirmPassword: '',
-    },
-    onSubmit: (values) => {
-      alert(JSON.stringify(values, null, 2))
-    },
-  })
+// const UpdatePassword = () => {
+//   const { updatePassword, error, isLoading } = useUpdatePassword()
+//   const [showPassword, setShowPassword] = useState(false)
 
-  const update = () => {
-    try {
-      updatePassword(
-        formik.values.oldPassword,
-        formik.values.newPassword,
-        formik.values.confirmPassword
-      )
-    } catch (error) {}
-  }
-  const { t } = useTranslation()
-  return (
-    <Gutter>
-      <div className="z-10 flex min-h-svh items-center justify-center pt-24 text-gray-200">
-        <form
-          className="mx-4 flex w-full flex-col gap-4 rounded-xl bg-neutral-950 px-6 py-8 shadow shadow-neutral-500 xs:w-3/4 sm:mx-0 sm:w-1/2 md:w-[28rem]"
-          onSubmit={formik.handleSubmit}
-        >
-          <h3 className="texl-lg mb-4 text-center font-bold md:text-xl">
-            {t("Parol Yangilash")}
-          </h3>
-          <div>
-            <label
-              className="mb-2 block text-sm font-bold text-neutral-300"
-              htmlFor="oldPassword"
-            >
-              {t("Eski parol")}
-            </label>
-            <input
-              id="oldPassword"
-              name="oldPassword"
-              className="auth-input"
-              type="password"
-              onChange={formik.handleChange}
-              value={formik.values.oldPassword}
-            />
-          </div>
-          <div>
-            <label
-              className="mb-2 block text-sm font-bold text-neutral-300"
-              htmlFor="newPassword"
-            >
-              {t("Yangi parol")}
-            </label>
-            <input
-              id="newPassword"
-              name="newPassword"
-              className="auth-input"
-              type="password"
-              onChange={formik.handleChange}
-              value={formik.values.newPassword}
-            />
-          </div>
-          <div>
-            <label
-              className="mb-2 block text-sm font-bold text-neutral-300"
-              htmlFor="confirmPassword"
-            >
-              {t("Yangi parolni qayta kiriting")}
-            </label>
-            <input
-              id="confirmPassword"
-              name="confirmPassword"
-              className="auth-input"
-              type="password"
-              onChange={formik.handleChange}
-              value={formik.values.confirmPassword}
-            />
-          </div>
-          <button
-            className="mt-4 w-full rounded-sm border border-primary bg-neutral-900 py-3 font-semibold transition-all hover:bg-black"
-            type="submit"
-            onClick={() => update()}
-          >
-            {t("O‘zgarishlarni saqlash")}
-          </button>
-        </form>
-      </div>
-    </Gutter>
-  )
-}
-export default UpdatePassword
+//   const formik = useFormik({
+//     initialValues: {
+//       oldPassword: '',
+//       newPassword: '',
+//       confirmPassword: '',
+//     },
+//     onSubmit: (values) => {
+//       alert(JSON.stringify(values, null, 2))
+//     },
+//   })
+
+//   const update = () => {
+//     try {
+//       updatePassword(
+//         formik.values.oldPassword,
+//         formik.values.newPassword,
+//         formik.values.confirmPassword
+//       )
+//     } catch (error) {}
+//   }
+//   const { t } = useTranslation()
+//   return (
+//     <Gutter>
+//       <div className="z-10 flex min-h-svh items-center justify-center pt-24 text-gray-200">
+//         <form
+//           className="mx-4 flex w-full flex-col gap-4 rounded-xl bg-neutral-950 px-6 py-8 shadow shadow-neutral-500 xs:w-3/4 sm:mx-0 sm:w-1/2 md:w-[28rem]"
+//           onSubmit={formik.handleSubmit}
+//         >
+//           <h3 className="texl-lg mb-4 text-center font-bold md:text-xl">
+//             {t("Parol Yangilash")}
+//           </h3>
+//           <div>
+//             <label
+//               className="mb-2 block text-sm font-bold text-neutral-300"
+//               htmlFor="oldPassword"
+//             >
+//               {t("Eski parol")}
+//             </label>
+//             <input
+//               id="oldPassword"
+//               name="oldPassword"
+//               className="auth-input"
+//               type="password"
+//               onChange={formik.handleChange}
+//               value={formik.values.oldPassword}
+//             />
+//           </div>
+//           <div>
+//             <label
+//               className="mb-2 block text-sm font-bold text-neutral-300"
+//               htmlFor="newPassword"
+//             >
+//               {t("Yangi parol")}
+//             </label>
+//             <input
+//               id="newPassword"
+//               name="newPassword"
+//               className="auth-input"
+//               type="password"
+//               onChange={formik.handleChange}
+//               value={formik.values.newPassword}
+//             />
+//           </div>
+//           <div>
+//             <label
+//               className="mb-2 block text-sm font-bold text-neutral-300"
+//               htmlFor="confirmPassword"
+//             >
+//               {t("Yangi parolni qayta kiriting")}
+//             </label>
+//             <input
+//               id="confirmPassword"
+//               name="confirmPassword"
+//               className="auth-input"
+//               type="password"
+//               onChange={formik.handleChange}
+//               value={formik.values.confirmPassword}
+//             />
+//           </div>
+//           <button
+//             className="mt-4 w-full rounded-sm border border-primary bg-neutral-900 py-3 font-semibold transition-all hover:bg-black"
+//             type="submit"
+//             onClick={() => update()}
+//           >
+//             {t("O‘zgarishlarni saqlash")}
+//           </button>
+//         </form>
+//       </div>
+//     </Gutter>
+//   )
+// }
+// export default UpdatePassword
