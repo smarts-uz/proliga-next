51ec01212e662543a0124dce8f2ab365dc9a9b2b
Ramz001 <rkenjaev1@gmail.com>
2024-10-28 12:20:28 +0500

    ad modal made responsive

--- src/components/AdModal/index.jsx
+++ src/components/AdModal/index.jsx
@@ -13,16 +13,16 @@ const AdModal = ({ isModalOpen, setModalOpen }) => {
       <DialogTrigger className="hidden">Ad Trigger</DialogTrigger>
       <DialogContent
         closeButtonStyle="right-0 -top-8"
-        className="w-[96%] max-w-max rounded-sm p-0 2xl:w-full"
+        className="max-h-max w-[96%] max-w-max rounded-sm p-0 2xl:w-full"
       >
-        <div className="block rounded-sm border border-blue-900 2xl:h-[720px] 2xl:w-[1280px]">
+        <div className="block md:min-w-[620px] lg:min-w-[960px] xl:min-w-[1024px] 2xl:min-w-[1280px]">
           <img
             src="/images/banner-desktop.jpg"
             alt="banner"
             width={128}
             height={72}
             loading="lazy"
-            className="h-full w-full bg-cover"
+            className="aspect-video h-full w-full rounded-sm"
           />
         </div>
         <DialogTitle className="hidden">Ad title</DialogTitle>
