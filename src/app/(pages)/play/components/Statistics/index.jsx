56dbc9a5f484f7f0f3b6dc0412be93da0f993203
Ramz001 <rkenjaev1@gmail.com>
2024-11-01 20:55:36 +0500

    auth bug fix & file upload functionality

--- src/app/(pages)/play/components/Statistics/index.jsx
+++ src/app/(pages)/play/components/Statistics/index.jsx
@@ -7,7 +7,7 @@ const Statistics = () => {
 
   return (
     <section className="flex w-full flex-col gap-2 lg:flex-row">
-      <div className="flex h-full min-h-[40rem] w-full flex-1 table-auto flex-col gap-4 overflow-x-auto rounded-2xl bg-black px-2 py-4 text-neutral-200 xs:px-3 md:p-5 lg:w-2/3">
+      <div className="flex h-full min-h-[40rem] w-full flex-1 table-auto flex-col gap-4 overflow-x-auto rounded-2xl bg-black px-2 py-3 text-neutral-200 xs:px-3 md:p-5 lg:w-2/3">
         {isLoading ? (
           <div className="flex h-full w-full items-center justify-center">
             <div className="loader" />
