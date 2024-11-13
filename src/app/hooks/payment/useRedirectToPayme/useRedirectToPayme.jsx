0cc276ba95f2149bf5e394273b73778dd39d0c1b
Ramz001 <rkenjaev1@gmail.com>
2024-11-13 18:10:14 +0500

    payme integration & restructure redirect pays to hooks

---
+++ src/app/hooks/payment/useRedirectToPayme/useRedirectToPayme.jsx
@@ -0,0 +1,51 @@
+import { useState } from 'react'
+import { toast } from 'react-toastify'
+import { useRouter } from 'next/navigation'
+import { useSelector } from 'react-redux'
+
+export const useRedirectToPayme = () => {
+  const router = useRouter()
+  const [error, setError] = useState(null)
+  const [isLoading, setIsLoading] = useState(false)
+  const { lang } = useSelector((store) => store.systemLanguage)
+  const { userTable } = useSelector((store) => store.auth)
+
+  const redirectToPayme = ({ amount, return_url }) => {
+    setIsLoading(false)
+    setError(null)
+
+    if (!userTable?.id) {
+      setError('User not found')
+      toast.error('User not found', { theme: 'dark' })
+      return
+    }
+
+    if (!amount) {
+      setError('Amount is required')
+      toast.error('Amount is required', { theme: 'dark' })
+      return
+    }
+
+    try {
+      setIsLoading(true)
+
+      const url = new URL('https://checkout.paycom.uz')
+      const m = process.env.NEXT_PUBLIC_PAYME_ID // merchant id
+      const ac = userTable?.id // account
+      const a = amount * 100 // amount
+      const l = lang
+      const cr = 4217 // UZS
+      const ct = 15000
+      const encoded = btoa(
+        `m=${m};ac.user_id=${ac};a=${a};l=${l};c=${return_url};ct=${ct};cr=${cr};`
+      )
+      router.push(url.href + encoded)
+    } catch (error) {
+      setError(error.message)
+      toast.error(error.message, { theme: 'dark' })
+    } finally {
+      setIsLoading(false)
+    }
+  }
+  return { redirectToPayme, isLoading, error }
+}
