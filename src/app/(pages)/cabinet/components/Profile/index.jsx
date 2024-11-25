c378b8355d536e76fa2703eea579d6482273330d
Ramz001 <rkenjaev1@gmail.com>
2024-11-25 17:36:26 +0500

    dynamically serving uploaded images

--- src/app/(pages)/cabinet/components/Profile/index.jsx
+++ src/app/(pages)/cabinet/components/Profile/index.jsx
@@ -52,8 +52,8 @@ const CabinetProfileTab = ({ setSettingsTab }) => {
             </span>
           )}
           {userTable?.email && userTable?.photo && (
-            <img
-              src={process.env.NEXT_PUBLIC_URL + userTable?.photo}
+            <Image
+              src={userTable?.photo}
               alt="user"
               width={32}
               draggable={false}
