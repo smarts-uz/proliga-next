82e3199f49f6a64f2cbdd9fb2040c6e7a3e5bc6e
Ramz001 <rkenjaev1@gmail.com>
2024-11-25 13:54:17 +0500

    upload single file mvp set up

---
+++ src/app/action/upload/saveFile.action.jsx
@@ -0,0 +1,33 @@
+'use server'
+
+import { join } from 'path'
+import { writeFile, mkdir } from 'fs/promises'
+import { existsSync } from 'fs'
+import mime from 'mime'
+
+export async function saveFile(formData) {
+  const file = formData.getAll('files')[0] // getting the first file from files
+  const dir = formData.get('dir')
+  const subDir = formData.get('subDir')
+
+  const bytes = await file.arrayBuffer()
+  const buffer = Buffer.from(bytes)
+  const extension = mime.getExtension(file.type)
+  const path = join(
+    process.cwd(),
+    `/public/${dir}/${subDir}`,
+    `image.${extension}`
+  )
+  const dirExists = existsSync(join(process.cwd(), `/public/${dir}`, subDir))
+
+  if (!dirExists) {
+    mkdir(join(process.cwd(), `/public/${dir}`, subDir), { recursive: true })
+  }
+
+  await writeFile(path, buffer)
+
+  return {
+    status: 201,
+    body: path,
+  }
+}
--- src/components/UploadFileModal/Uploader/index.jsx
+++ src/components/UploadFileModal/Uploader/index.jsx
@@ -10,8 +10,8 @@ import { useState } from 'react'
 import Uppy from '@uppy/core'
 import { useDispatch, useSelector } from 'react-redux'
 import { LANGUAGE } from 'app/utils/languages.util'
-import UppyServerActionUpload from 'app/plugins/uppy/UppyServerActionUpload'
-import { saveFiles } from 'app/action/upload/route'
+import UppyServerActionUpload from 'app/plugins/UploadFile/UppyServerActionUpload'
+import { saveFile } from 'app/action/upload/saveFile.action'
 import { useUpdateUserPhoto } from 'app/hooks/user/useUpdateUserPhoto/useUpdateUserPhoto'
 
 export const UppyUploader = () => {
@@ -19,53 +19,28 @@ export const UppyUploader = () => {
   const { lang } = useSelector((state) => state.systemLanguage)
   const { userTable } = useSelector((state) => state.auth)
   const { updateUserPhoto } = useUpdateUserPhoto()
-  const [fileTypes, setFileTypes] = useState([])
-  const [fileNames, setFileNames] = useState([])
 
-  const [uppy] = useState(() =>
-    new Uppy({
-      autoProceed: false,
-      allowMultipleUploadBatches: false,
-      locale: lang === LANGUAGE.uz ? Uzbek : Russian,
-      restrictions: {
-        maxFileSize: 5242880, //5mb
-        allowedFileTypes: ['image/png', 'image/jpeg', 'image/webp'],
-        maxNumberOfFiles: 1,
-        minNumberOfFiles: 1,
-      },
-    })
-      .on('file-added', (result) => {
-        setFileTypes((prev) => [...prev, result?.data?.type])
-        setFileNames((prev) => [...prev, userTable?.id.toString()])
+  const [uppy] = useState(
+    () =>
+      new Uppy({
+        autoProceed: false,
+        allowMultipleUploadBatches: false,
+        locale: lang === LANGUAGE.uz ? Uzbek : Russian,
+        restrictions: {
+          maxFileSize: 5 * 1024 * 1024, // 5 mb
+          allowedFileTypes: ['image/png', 'image/jpeg'],
+          maxNumberOfFiles: 1,
+          minNumberOfFiles: 1,
+        },
+      }).use(UppyServerActionUpload, {
+        action: saveFile,
+        dir: 'user',
+        subDir: userTable?.id.toString() || '',
       })
-      // .on('upload-success', () => {
-      //   setFileTypes([])
-      //   setFileNames([])
-      // })
-      // .on('upload-error', () => {
-      //   setFileTypes([])
-      //   setFileNames([])
-      // })
-      .use(UppyServerActionUpload, {
-        action: saveFiles,
-        dir: 'locales',
-        folderNames: fileNames || [],
-      })
+    // .on('upload-success', () => {
+    //   setFileTypes([])
+    // })
   )
-  // .on('upload-success', () => {
-  //   updateUserPhoto()
-  // })
 
-  //   onBeforeFileAdded: (currentFile) => {
-  //     const modifiedFile = {
-  //       ...currentFile,
-  //       name: `${userTable?.id}.${currentFile.extension}`,
-  //     }
-  //     modifiedFile.name &&
-  //       dispatch(setUserTempData({ photo: modifiedFile.name }))
-
-  //     return modifiedFile
-  //   },
-  // })
   return <Dashboard className="w-full rounded-xl" theme="dark" uppy={uppy} />
 }
