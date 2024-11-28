9fab086d4b517d7ab98c9c3f145947be1d6c40b4
Ramz001 <rkenjaev1@gmail.com>
2024-11-28 20:14:07 +0500

    added trycatch blog for save file action

--- src/app/action/upload/saveFile.action.jsx
+++ src/app/action/upload/saveFile.action.jsx
@@ -20,16 +20,23 @@ export async function saveFile(formData) {
   )
   const dirExists = existsSync(join(process.cwd(), `/public/${dir}`, subDir))
 
-  if (dirExists) {
-    await writeFile(path, buffer)
-  } else {
-    await mkdir(join(process.cwd(), `/public/${dir}`, subDir), {
-      recursive: true,
-    }).then(async () => await writeFile(path, buffer))
-  }
+  try {
+    if (dirExists) {
+      await writeFile(path, buffer)
+    } else {
+      await mkdir(join(process.cwd(), `/public/${dir}`, subDir), {
+        recursive: true,
+      }).then(async () => await writeFile(path, buffer))
+    }
 
-  return {
-    status: 201,
-    body: path,
+    return {
+      status: 201,
+      body: path,
+    }
+  } catch (error) {
+    return {
+      status: 500,
+      body: error,
+    }
   }
 }
