83019732fa138afc612e13409ca32be015d9bd20
Ramz001 <rkenjaev1@gmail.com>
2024-11-30 16:05:40 +0500

    work on yandex modal fix

--- src/components/Banners/Modal/index.jsx
+++ src/components/Banners/Modal/index.jsx
@@ -29,34 +29,38 @@ const ModalBanner = ({ isModalOpen, setModalOpen }) => {
   }
 
   return (
-    <Dialog open={isModalOpen} onOpenChange={handleOpen}>
-      <DialogTrigger className="hidden">Ad Trigger</DialogTrigger>
-      <DialogContent
-        closeButtonStyle="right-0 -top-8"
-        className="max-h-max w-[96%] rounded-md p-0 md:max-w-[80%] 2xl:w-full 2xl:max-w-[1280px]"
-      >
-        {banner?.type === BANNER_SERVICE_TYPE.CUSTOM && (
-          <Link
-            href={banner?.link ?? ''}
-            className="block rounded md:min-w-[620px] xl:min-w-[1024px] 2xl:min-w-[1280px] 2xl:max-w-[1280px]"
+    <>
+      {banner?.type === BANNER_SERVICE_TYPE.CUSTOM && (
+        <Dialog open={isModalOpen} onOpenChange={handleOpen}>
+          <DialogTrigger className="hidden">Ad Trigger</DialogTrigger>
+          <DialogContent
+            closeButtonStyle="right-0 -top-8"
+            className="max-h-max w-[96%] rounded-md p-0 md:max-w-[80%] 2xl:w-full 2xl:max-w-[1280px]"
           >
-            <img
-              src={banner?.content_url ?? ''}
-              alt={banner?.name}
-              width={128}
-              height={72}
-              loading="lazy"
-              className="aspect-video h-full w-full rounded"
-            />
-          </Link>
-        )}
-        {banner?.type === BANNER_SERVICE_TYPE.YANDEX && (
-          <YandexAd blockId={banner?.service_id} />
-        )}
-        <DialogTitle className="hidden">Ad title</DialogTitle>
-        <DialogDescription className="hidden">Ad Descriptor</DialogDescription>
-      </DialogContent>
-    </Dialog>
+            <Link
+              href={banner?.link ?? ''}
+              className="block rounded md:min-w-[620px] xl:min-w-[1024px] 2xl:min-w-[1280px] 2xl:max-w-[1280px]"
+            >
+              <img
+                src={banner?.content_url ?? ''}
+                alt={banner?.name}
+                width={128}
+                height={72}
+                loading="lazy"
+                className="aspect-video h-full w-full rounded"
+              />
+            </Link>
+            <DialogTitle className="hidden">Ad title</DialogTitle>
+            <DialogDescription className="hidden">
+              Ad Descriptor
+            </DialogDescription>
+          </DialogContent>
+        </Dialog>
+      )}
+      {banner?.type === BANNER_SERVICE_TYPE.YANDEX && (
+        <YandexAd blockId={banner?.service_id} />
+      )}
+    </>
   )
 }
 
